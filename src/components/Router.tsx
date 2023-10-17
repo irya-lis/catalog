7b7bc757ee68edbf2778cc550a85ce51230dd66f
Irina Rakova <lis.ovskaya1119@gmail.com>
2023-10-17 17:28:15 +0300

    delete basename

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./screens/home/Home";
 import IceCreamDetail from "./screens/iceCream-detail/IceCreamDetail";
-const basename = process.env.PUBLIC_URL || "/";
 
 const Router = () => {
     return (
-        <BrowserRouter basename={basename}>
+        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/iceCream/:id" element={<IceCreamDetail/>}/>
